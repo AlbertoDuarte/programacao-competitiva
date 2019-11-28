1248218f83b88ff946f55671ff93abd80bd00936
Alberto Duarte <albertotdneto@hotmail.com>
2019-11-28 13:22:04 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 #define pii pair<int, int>
 #define vi vector<int>
+#define vii vector<pii>
 #define ll long long
 #define pb push_back
+#define mp make_pair
 #define f first
 #define s second
-
 using namespace std;
