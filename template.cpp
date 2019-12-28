6d26abb60f5c12f5ee57b5c3177b08f07b94d6b6
Alberto Duarte <albertotdneto@hotmail.com>
2019-12-28 20:48:24 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
+#define ll long long
 #define pii pair<int, int>
 #define vi vector<int>
+#define vl vector<ll>
 #define vii vector<pii>
-#define ll long long
 #define pb push_back
 #define mp make_pair
 #define f first
 #define s second
+#define forn(i, n) for(int i=0; i<(int)(n); i++)
+#define forne(i, a, b) for(int i=a; i<=(int)(b); i++)
+
+#define gottagofast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n";
 using namespace std;
