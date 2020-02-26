2de51d57aef8108f6adf501cb92a4a1f0cc2a2b0
Alberto Duarte <albertotdneto@hotmail.com>
2020-02-26 12:44:43 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
+#define ff first
+#define ss second
 #define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
 #define pii pair<int, int>
 #define vi vector<int>
-#define vl vector<ll>
 #define vii vector<pii>
-#define pb push_back
-#define mp make_pair
-#define f first
-#define s second
-#define forn(i, n) for(int i=0; i<(int)(n); i++)
-#define forne(i, a, b) for(int i=a; i<=(int)(b); i++)
-
-#define gottagofast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define endl "\n";
+#define sws ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+#define teto(a, b) ((a+b-1)/(b))
 using namespace std;
+
+const int MAX = 400010;
+const int MOD = 1000000007;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const ld EPS = 1e-7;
+
+// Extra
+#define forn(i, n) for(int i = 0; i < (int)n; i++)
+#define forne(i, a, b) for(int i = a; i <= b; i++)
+//
